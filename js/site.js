1672c8df83b694e70950897e4d698df9dc9db983
Bhumika <bpatel63@hawk.iit.edu>
2018-11-16 18:27:58 -0600

    updating site.js
    javascript for submitting hte form

--- js/site.js
+++ js/site.js
@@ -1,4 +1,34 @@
 (function () {
 	function validateForm() {
 		var nameField = document.forms["signup-form"["fname"].value;
+		var nameField = document.forms["signup-form"["lname"].value;
+		var emailField = document.forms["signup-form"["email"].value;
+		var phoneField = document.forms["signup-form"["phone"].value;
+
+		if (nameField === "") {
+            alert("All information must be filled out");
+            return false;
+        }
 		
+		if (nameField === "") {
+            alert("All information must be filled out");
+            return false;
+        }
+
+        if (emailField === "") {
+            alert("All information must be filled out");
+            return false;
+        }
+
+        if (phoneField === "") {
+            alert("All information must be filled out");
+            return false;
+        }
+	}
+	
+document.forms["signup-form"].addEventListener('submit', validateForm);
+
+/*(function reset() {
+
+document.forms.getElementById("signup-form").reset();
+}*/
